5ac849b2582bf703ceebc118be5d60aff447e23c
Kavya-Papisetty <kavyapapisetty3@gmail.com>
2022-03-07 21:11:56 +0530

    Initial commit for the project

---
+++ library/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime,timedelta
+
+class StudentExtra(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    enrollment = models.CharField(max_length=40)
+    branch = models.CharField(max_length=40)
+    #used in issue book
+    def __str__(self):
+        return str(self.user) + "["+str(self.enrollment)+']'
+    @property
+    def get_name(self):
+        return self.user.first_name
+    @property
+    def getuserid(self):
+        return self.user.id
+
+# class category(models.Model):
+     
+#     name = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return str(self.name) 
+
+
+
+class Book(models.Model):
+    catchoice= [
+        ('education', 'Education'),
+        ('entertainment', 'Entertainment'),
+        ('comics', 'Comics'),
+        ('biography', 'Biography'),
+        ('history', 'History'),
+        ('novel', 'Novel'),
+        ('fantasy', 'Fantasy'),
+        ('thriller', 'Thriller'),
+        ('romance', 'Romance'),
+        ('scifi','Sci-Fi')
+        ]
+    name=models.CharField(max_length=500)
+    isbn=models.PositiveIntegerField()
+    author=models.CharField(max_length=200)
+    category=models.CharField(max_length=200,choices=catchoice,default='education')
+
+    def __str__(self):
+        return str(self.name) +"["+str(self.isbn)+']'
+
+
+def get_expiry():
+    return datetime.today() + timedelta(days=15)
+class IssuedBook(models.Model):
+    #moved this in forms.py
+    #enrollment=[(student.enrollment,str(student.get_name)+' ['+str(student.enrollment)+']') for student in StudentExtra.objects.all()]
+    # enrollment=models.CharField(max_length=30)
+    student = models.ForeignKey(StudentExtra,on_delete=models.SET_NULL,null=True,blank=True)
+    #isbn=[(str(book.isbn),book.name+' ['+str(book.isbn)+']') for book in Book.objects.all()]
+    # isbn=models.CharField(max_length=30)
+    book = models.ForeignKey(Book,on_delete=models.CASCADE,null=True,blank=True)
+    issuedate=models.DateField(auto_now=True)
+    expirydate=models.DateField(default=get_expiry)
+    def __str__(self):
+        return str(self.student)
+
+# class Student(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     classroom = models.CharField(max_length=10)
+#     branch = models.CharField(max_length=10)
+#     roll_no = models.CharField(max_length=3, blank=True)
+#     phone = models.CharField(max_length=10, blank=True)
+#     image = models.ImageField(upload_to="", blank=True)
+
+#     def __str__(self):
+#         return str(self.user) + " ["+str(self.branch)+']' + " ["+str(self.classroom)+']' + " ["+str(self.roll_no)+']'
+
+
+def expiry():
+    return datetime.today() + timedelta(days=14)
+
+
---
+++ library/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path
+from . import views
+from django.urls import path,include
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    
+    path('', views.home_view),
+
+    path('adminclick', views.adminclick_view, name = 'adminclick'),
+    path('studentclick', views.studentclick_view, name = 'studentclick'),
+
+
+    path('adminsignup', views.adminsignup_view, name = 'adminsignup'),
+    path('studentsignup', views.studentsignup_view, name = 'studentsignup'),
+    path('adminlogin', LoginView.as_view(template_name='library/adminlogin.html'), name = 'adminlogin'),
+    path('studentlogin', LoginView.as_view(template_name='library/studentlogin.html'), name = 'studentlogin'),
+
+    path('logout', LogoutView.as_view(template_name='library/index.html'), name = 'logout'),
+    path('afterlogin', views.afterlogin_view, name = 'afterlogin'),
+
+    path('addbook', views.addbook_view, name = 'addbook'),
+    path('viewbook', views.viewbook_view, name = 'viewbook'),
+    path('issuebook', views.issuebook_view, name = 'issuebook'),
+    path('viewissuedbook', views.viewissuedbook_view, name = 'viewissuedbook'),
+    path('viewstudent', views.viewstudent_view, name ='viewstudent'),
+    path('viewissuedbookbystudent', views.viewissuedbookbystudent, name = 'viewissuedbookbystudent'),
+
+    path('aboutus', views.aboutus_view, name = 'aboutus'),
+    path('contactus', views.contactus_view, name = 'contactus'),
+    path('delete/<str:pk>/', views.delete_book, name = 'delete'),
+    path('update', views.Update, name = 'update'),
+    path('reset_password/',auth_views.PasswordResetView.as_view(),name="reset_password"),
+
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+   
+]
