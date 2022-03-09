75cee6f5d282c14d0f794433569e53092d535ea8
Kavya-Papisetty <96787473+Kavya-Papisetty@users.noreply.github.com>
2022-03-09 21:38:31 +0530

    Update views.py

--- library/views.py
+++ library/views.py
@@ -1,3 +1,7 @@
+from pyexpat.errors import messages
+from time import timezone
+from tkinter.tix import Form
+from django.conf import settings
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from . import forms,models
@@ -8,7 +12,9 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 from datetime import datetime,timedelta,date
 from django.core.mail import send_mail
 from librarymanagementsystem.settings import EMAIL_HOST_USER
-
+from .models import Fine, IssuedBook 
+import razorpay
+from .utilities import calcFine
 
 def home_view(request):
     if request.user.is_authenticated:
@@ -101,6 +107,7 @@ def addbook_view(request):
 @user_passes_test(is_admin)
 def viewbook_view(request):
     books=models.Book.objects.all()
+
     return render(request,'library/viewbook.html',{'books':books})
 
 
@@ -114,12 +121,12 @@ def issuebook_view(request):
         #now this form have data from html
         form=forms.IssuedBookForm(request.POST)
         if form.is_valid():
-            obj=models.IssuedBook()
-            obj.enrollment=request.POST.get('enrollment2')
-            obj.isbn=request.POST.get('isbn2')
-            obj.save()
+            # obj=models.IssuedBook()
+            # obj.enrollment=request.POST.get('enrollment2')
+            # obj.isbn=request.POST.get('isbn2')
+            form.save()
             return render(request,'library/bookissued.html')
-            user.save()
+           
 
     return render(request,'library/issuebook.html',{'form':form})
 
@@ -141,7 +148,9 @@ def viewissuedbook_view(request):
             day=d-15
             fine=day*10
 
-            models.IssuedBook.save()
+            # models.IssuedBook.save()
+            
+            
 
 
         # books=list(models.Book.objects.filter(isbn=ib.isbn))
@@ -166,6 +175,7 @@ def viewstudent_view(request):
 def viewissuedbookbystudent(request):
     # student=models.StudentExtra.objects.filter(user_id=request.user.id)
     issuedbook=models.IssuedBook.objects.filter(student__user__id = request.user.id)
+    
 
     li1=[]
 
@@ -262,3 +272,82 @@ def Update(request):
 
 # def Profile(request):
 #     return render(request, 'profile.html')
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def myfines(request):
+    if models.StudentExtra.objects.filter(student_id=request.user_id):
+        student=models.StudentExtra.objects.filter(student_id=request.user_id)[0]
+        issues=models.IssuedBook.objects.filter(student=student)
+        for issue in issues:
+            calcFine(issue)
+        fines=Fine.objects.filter(student=student)
+        return render(request,'library/myfines.html',{'fines':fines})
+    messages.error(request,'You are Not a Student !')
+    return redirect('/')
+
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u:  u.is_superuser ,login_url='/admin/')
+def allfines(request):
+    issues=IssuedBook.objects.all()
+    for issuebook in issues:
+        calcFine(issuebook)
+    return redirect('/admin/library/fine/')
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u:  u.is_superuser ,login_url='/admin/')
+def deletefine(request,fineID):
+    fine=Fine.objects.get(id=fineID)
+    fine.delete()
+    return redirect('/all-fines/')
+
+
+razorpay_client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def payfine(request,fineID):
+    fine=Fine.objects.get(id=fineID)
+    order_amount = int(fine.amount)*100
+    order_currency = 'INR'
+    order_receipt = fine.order_id
+    
+    
+    razorpay_order=razorpay_client.order.create(dict(amount=order_amount, currency=order_currency, receipt=order_receipt, ))
+    print(razorpay_order)
+    
+    
+    return render(request,'library/payfine.html',
+    {'amount':order_amount,'razor_id':settings.RAZORPAY_KEY_ID,
+    'reciept':razorpay_order['id'],
+    'amount_displayed':order_amount / 100,
+    'address':'a custom address',
+    'fine':fine, 
+    })
+
+
+@login_required(login_url='/student/login/')
+@user_passes_test(lambda u: not u.is_superuser ,login_url='/student/login/')
+def pay_status(request,fineID):
+    if request.method == 'POST':
+        params_dict={
+            'razorpay_payment_id':request.POST['razorpay_payment_id'],
+            'razorpay_order_id':request.POST['razorpay_order_id'],
+            'razorpay_signature':request.POST['razorpay_signature'],
+        }
+        try:
+            status=razorpay_client.utility.verify_payment_signature(params_dict)
+            if status is None:
+                fine=Fine.objects.get(id=fineID)
+                fine.paid=True
+                fine.datetime_of_payment=timezone.now()
+                fine.razorpay_payment_id=request.POST['razorpay_payment_id']
+                fine.razorpay_signature=request.POST['razorpay_signature']
+                fine.razorpay_order_id = request.POST['razorpay_order_id']
+                fine.save()
+                
+            messages.success(request,'Payment Succesfull')
+        except:
+            messages.error(request,'Payment Failure')
+    return redirect('/my-fines/')
