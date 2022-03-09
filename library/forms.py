d0eb21fa082a5755c7a06797064b42719d32e654
Kavya-Papisetty <96787473+Kavya-Papisetty@users.noreply.github.com>
2022-03-09 21:29:30 +0530

    Update forms.py

--- library/forms.py
+++ library/forms.py
@@ -31,11 +31,13 @@ class BookForm(forms.ModelForm):
     class Meta:
         model=models.Book
         fields=['name','isbn','author','category']
-class IssuedBookForm(forms.Form):
+class IssuedBookForm(forms.ModelForm):
     #to_field_name value will be stored when form is submitted.....__str__ method of book model will be shown there in html
-    isbn2=forms.ModelChoiceField(queryset=models.Book.objects.all(),empty_label="Name and isbn", to_field_name="isbn",label='Name and Isbn')
-    enrollment2=forms.ModelChoiceField(queryset=models.StudentExtra.objects.all(),empty_label="Name and enrollment",to_field_name='enrollment',label='Name and enrollment')
-    
+    # isbn2=forms.ModelChoiceField(queryset=models.Book.objects.all(),empty_label="Name and isbn", to_field_name="isbn",label='Name and Isbn')
+    # enrollment2=forms.ModelChoiceField(queryset=models.StudentExtra.objects.all(),empty_label="Name and enrollment",to_field_name='enrollment',label='Name and enrollment')
+    class Meta:
+        model = models.IssuedBook
+        fields = ['student','book']
 # class ProjectForm(forms.ModelForm):
 #     class Meta:
 #         model = models.Student
